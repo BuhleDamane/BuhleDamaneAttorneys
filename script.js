fb91c656079031c22c6b06fb5236b0c41d155f71
BuhleDamane <ziziphodamane@gmail.com>
2024-09-14 13:19:51 +0200

    updated all my files

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const currentPage = window.location.pathname.split('/').pop();
+    const links = document.querySelectorAll('.nav-link');
 
-const currentPage = window.location.pathname.split('/').pop();
-const links = document.querySelectorAll('.nav-link');
-
-links.forEach(link => {
-    const linkHref = link.getAttribute('href');
-    if (linkHref === currentPage) {
-        link.classList.add('active');
-    }
+    links.forEach(link => {
+        const linkHref = link.getAttribute('href');
+        if (linkHref === currentPage) {
+            link.classList.add('active');
+        }
+    });
 });
-
